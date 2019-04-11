5941f4af6fb8232d3d923fdd997338a9a73793c4
limitless2014 <37983571+limitless2014@users.noreply.github.com>
2019-04-11 17:55:35 +0430

    adding react-native-tts

--- android/app/src/main/java/com/cafefrench2/MainApplication.java
+++ android/app/src/main/java/com/cafefrench2/MainApplication.java
@@ -3,6 +3,7 @@ package com.cafefrench2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new TextToSpeechPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage()
       );
